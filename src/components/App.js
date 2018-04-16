b1d730dddabc03b52c912ab7d2ecaeea80eca5b9
Mukesh Sharma <mukesh@bidchats-MacBook-Pro.local>
2018-04-16 11:28:15 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+class  extends Component {
+  render() {
+    return (
+      <View>React Native</View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default ;
